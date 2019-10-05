f5c23aa3e5fc319ac6c4c5f78ece81fdf54053ad
Tommi Lepola <lepola.tommi@gmail.com>
2019-10-05 14:26:48 +0300

    Add all required views

---
+++ WeatherApp/Controllers/ForecastViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Tommi Lepola on 05/10/2019.
+//  Copyright © 2019 Tommi Lepola. All rights reserved.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
